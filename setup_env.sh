02dfb4e52c8f0e20c39b1024553dc14392bf010b
dyk4ever0 <dyk4ever0@gmail.com>
2023-02-05 11:25:19 +0900

    translator, actions secrets updated

--- setup_env.sh
+++ setup_env.sh
@@ -4,3 +4,5 @@ echo TI_CLIENT_ID=$TI_CLIENT_ID >> .env
 echo TI_SECRET=$TI_SECRET >> .env
 echo TI_CODE=$TI_CODE >> .env
 echo TI_ACCESS_TOKEN=$TI_ACCESS_TOKEN >> .env
+echo PAPAGO_ID=$PAPAGO_ID >> .env
+echo PAPAGO_SECRET=$PAPAGO_SECRET >> .env
